8770689682dad71d8d390878199674b951fb928b
Dylan Alves Silva <dylan.alves-silva@epitech.eu>
2020-04-07 17:51:26 +0200

    skeleton project with the first component created for test

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FirstComponent extends Component {
+    render() {
+        return (
+            <div>
+                Here is the first component
+            </div>
+        )
+    }
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import FirstComponent from "../components/FirstComponent"
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <FirstComponent />
+            </div>
+        )
+    }
+}
