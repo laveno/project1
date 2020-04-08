7f4a3923291cff283f16a6e317685c9b27493320
Sylvie Veyssiere <robin.veyssiere@epitech.eu>
2020-04-08 19:56:53 +0200

    Added function to recuperate data from csv file

--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3';
+import data from './04-07-2020.csv';
 
 export default class FirstComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.convert = this.convert.bind(this);
+      }
+
+    convert() {
+        d3.csv(data, function(data) { console.log(data); });
+    }
+
     render() {
         return (
             <div>
                 Here is the first component
+                <button onClick={this.convert}>
+                  Click me!
+                </button>
             </div>
+            
         )
     }
 }
