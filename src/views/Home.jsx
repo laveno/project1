cca8a9966b44f5a62c7ac0479a59ab1afc2e9829
Sylvie Veyssiere <robin.veyssiere@epitech.eu>
2020-04-15 19:19:21 +0200

    Changed the way to get data now with d3 and URL to parse more easily

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,7 @@ import TotalConfirmed from "../components/TotalConfirmed"
 import MapChart from "../components/MapChart"
 import axios from 'axios'
 import { readString } from 'react-papaparse'
+import * as d3 from 'd3';
 
 
 export default class Home extends Component {
@@ -37,7 +38,16 @@ export default class Home extends Component {
             month = "0" + month
         var actual_date = month + "-" + date + "-" + year
         console.log(actual_date)
-        axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + actual_date + '.csv')
+ 
+        d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + actual_date + '.csv', function(error, data) {
+            if (error) {  
+                console.log(error);  //Log the error.
+            } else {      
+                console.log(data);   //Log the data.
+            }
+        });
+
+        /*axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + actual_date + '.csv')
         .then(res => {
             const data = res.data;
             console.log(res.data)
@@ -61,83 +71,8 @@ export default class Home extends Component {
             }
             console.log(JSON.stringify(result))
             //return JSON.stringify(result);
-           /* const parse = readString(res.data)
-            sstate.setState({ data: parse.data})
-            var tab = []
-            var i = 1
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][0])
-            }
-            sstate.setState({ FIPS: tab})
-            //console.log(sstate.state.FIPS)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][1])
-            }
-            sstate.setState({ Admin2: tab})
-            //console.log(sstate.state.Admin2)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][2])
-            }
-            sstate.setState({ Province_State: tab})
-            //console.log(sstate.state.Province_State)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][3])
-            }
-            sstate.setState({ Country_Region: tab})
-            //console.log(sstate.state.Country_Region)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][4])
-            }
-            sstate.setState({ Last_Update: tab})
-            //console.log(sstate.state.Last_Update)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][5])
-            }
-            sstate.setState({ Lat: tab})
-            //console.log(sstate.state.Lat)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][6])
-            }
-            sstate.setState({ Long: tab})
-            //console.log(sstate.state.Long)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][7])
-            }
-            sstate.setState({ Confirmed: tab})
-            //console.log(sstate.state.Confirmed)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][8])
-            }
-            sstate.setState({ Deaths: tab})
-            //console.log(sstate.state.Deaths)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][9])
-            }
-            sstate.setState({ Recovered: tab})
-            //console.log(sstate.state.Recovered)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][10])
-            }
-            sstate.setState({ Active: tab})
-            //console.log(sstate.state.Active)
-            tab = []
-            for (i = 1; parse.data[i]; i++) {
-                tab.push(parse.data[i][11])
-            }
-            sstate.setState({ Combined_Key: tab})*/
-            //console.log(sstate.state.Combined_Key)
         })
-        .catch(e => {console.log(e)});
+        .catch(e => {console.log(e)});*/
     }
 
     render() {
